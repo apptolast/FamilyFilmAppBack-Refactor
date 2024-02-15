7478fca94b6d4e69e959a1850604ea91c94091e7
Coshiloco <pablohurtadohg@gmail.com>
2024-02-16 00:14:18 +0100

    Probando

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from router import genres, groups, users
 
 app = FastAPI()
 
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 app.include_router(users.router)
 app.include_router(groups.router)
 app.include_router(genres.router)
