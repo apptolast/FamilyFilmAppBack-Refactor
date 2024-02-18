29f9ecc474751cac966a5ed1263f8bb565dcc36f
El3auti <elbautisocial@gmail.com>
2024-02-18 15:31:02 -0300

    create_groups finished

--- router/groups.py
+++ router/groups.py
@@ -1,27 +1,40 @@
 from fastapi import APIRouter,status,Depends
-from sqlalchemy.exc import IntegrityError
+from models import WatchList, ViewList
 from models.GroupUser import GroupUser
-from schema.Group import GroupCreate,AddUser
+from schema.Group import GroupCreate,AddUser,GroupData
 from config.db import session
 from models.Group import Group
 from controllers.users import auth_user
 from models.User import User
 from controllers.groups import get_group_all, get_group_by_id
 from controllers.session import add_to_db,delete_to_db
+from schema.Movie import ShowMovie, movieData
 
 router = APIRouter(
     prefix="/group",
     tags=["Groups"]
 )
 
-@router.post("/create", status_code=status.HTTP_201_CREATED, )
+@router.post("/create", status_code=status.HTTP_201_CREATED, response_model= GroupData )
 async def create_group(group: GroupCreate,me = Depends(auth_user)):
     db_group = Group(name= group.name)
     add_to_db(db_group)
-    db_groupUser = GroupUser(user_id = me.id, group_id = get_group_all()[-1].id)
+    group_id = get_group_all()[-1].id
+    db_groupUser = GroupUser(user_id = me.id, group_id =group_id)
     add_to_db(db_groupUser)
     user = session.query(User).filter(User.id == me.id).first()
-    return {"status": "success", "data": f"{db_group.name} and {db_groupUser.user_id} and {[group.group.name for group in user.groups]}"}
+    me_dict = {
+        "userId": me.id,
+        "email": me.email,
+        "firebaseUuid":"",
+        "role": me.role,
+    }
+    return GroupData(
+        id=group_id, 
+        name=group.name,  
+        user_owner_id=me.id,
+        users= [me_dict]
+    )
 
 @router.get('/all')
 async def get_groups(me = Depends(auth_user)):
