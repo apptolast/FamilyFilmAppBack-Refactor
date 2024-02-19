f5e0de45e97e0828186da8ce68a7814d266a1262
El3auti <126888203+El3auti@users.noreply.github.com>
2024-02-18 21:36:53 -0300

    Feature/FFA-122  Genre Endpoints (#5)
    * genre dowload data endpoint finished
    
    * all genre endpoints finished

--- controllers/genre.py
+++ controllers/genre.py
@@ -1,12 +1,15 @@
 from config.db import session
 from models.Genre import Genre
+from sqlalchemy import text
 
 
-def genre_filter(name):
-    return session.query(Genre).filter(Genre.name == name).first()
+def genre_filter(idiom,name):
+    return session.query(Genre).filter(text(f"genres.name->>'{idiom}' = :name")).params(name=name).first()
 
-def get_all_genres():
-    return session.query(Genre).all()
+def get_all_genres(idiom):
+    genres = session.query(Genre.id, text(f"genres.name->>'{idiom}'")).all()
+    return [{"id": genre[0], "name": genre[1]} for genre in genres]
 
-def get_genre_by_id(id):
-    return session.query(Genre).filter(Genre.id == id).first()
+def get_genre_by_id(id, idiom):
+    genre = session.query(Genre.id, text(f"genres.name->>'{idiom}'")).filter(Genre.id == id).first()
+    return {"id": genre[0], "name": genre[1]} if genre else None
