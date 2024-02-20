89103f0513967d7799eb49cc089499f39cffea06
Pablo Hurtado Gonzalo <79223844+Coshiloco@users.noreply.github.com>
2024-02-20 23:55:33 +0100

    Feature/movie endpoints (#7)
    * Dowloader Movies in progress
    
    * Docker mas descarga de peliculas
    
    * Fix
    
    * dowloadMovie optimized , fixing the filter in...
    
    * fixing problems #1
    
    * fixing #2
    
    ---------
    
    Co-authored-by: El3auti <elbautisocial@gmail.com>

--- models/Movie.py
+++ models/Movie.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, Float
+from sqlalchemy import JSON, Column, Integer, String, Boolean, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import ARRAY
 from models.base import Base
@@ -6,17 +6,22 @@ from models.GenreMovieAssociation import genre_movie_association
 
 class Movie(Base):
     __tablename__ = 'movies'
+
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     adult = Column(Boolean, default=False)
-    title = Column(String)
+    title = Column(JSON)
     genre_ids = Column(ARRAY(Integer))
     language = Column(String)
-    synopsis = Column(String, nullable=True)
+    synopsis = Column(JSON, nullable=True)
     image = Column(String, nullable=True)
-    release_date = Column(String, nullable=True)  # Ajustado a String para mantener la consistencia con Prisma
+    release_date = Column(String, nullable=True) 
     vote_average = Column(Float)
     vote_count = Column(Integer, nullable=True)
 
     genres = relationship('Genre', secondary=genre_movie_association, back_populates='movies')
     watchLists = relationship('WatchList', back_populates='movie')
     viewLists = relationship('ViewList', back_populates='movie')
+
+    #"overview": "After a deadly earthquake turns Seoul into a lawless badland, a fearless huntsman springs into action to rescue a teenager abducted by a mad doctor.",
+    # "title": "Badland Hunters",
--- router/genres.py
+++ router/genres.py
@@ -1,10 +1,13 @@
 from typing import List
 from fastapi import APIRouter
+from sqlalchemy import update
 from schema.Genre import ShowGenre
 from models.Genre import Genre
-from controllers.genre import genre_filter, get_all_genres, get_genre_by_id
+from controllers.genre import get_all_genres, get_genre_by_id
 from controllers.session import add_to_db
 from controllers.moviesapi import api,url_genre
+from config.db import session
+
 
 
 router = APIRouter(
@@ -14,13 +17,20 @@ router = APIRouter(
 
 @router.post("/dowloadgenre/{idiom:str}", status_code=201,response_model=List[ShowGenre])
 async def create_genre(idiom):
+
     json_response = api(f'{url_genre}{idiom}')
 
     for genre in json_response['genres']:
-        if genre_filter(idiom,genre['name']) == None:
+        existing_genre = session.query(Genre).filter(Genre.id == genre['id']).first()
+        
+        if existing_genre is None:
            add_to_db(Genre(id=genre['id'],name={f"{idiom}":genre['name']}))
+        else:
+            existing_genre.name = {**existing_genre.name, idiom: genre['name']}
+            session.commit()
+        
     
-    return get_all_genres()
+    return get_all_genres(idiom)
 
 @router.get('/all/{idiom:str}',status_code=200, response_model=List[ShowGenre])
 async def get_genres(idiom:str):
