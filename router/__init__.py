930e74f02d8d932de8806ff25359b24d18e91b47
Pablo Hurtado Gonzalo <79223844+Coshiloco@users.noreply.github.com>
2024-03-10 21:15:25 +0100

    Fix (#14)

--- router/__init__.py
+++ router/__init__.py
@@ -2,4 +2,4 @@ from .genres import router as genres
 from .groups import router as groups
 from .users import router as users
 from .movies import router as movies
-from .users import router as users
+
