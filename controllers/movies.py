dd2afa4ebd973c8e6269f3caed376b4eff3cd073
El3auti <126888203+El3auti@users.noreply.github.com>
2024-04-15 13:47:33 -0300

    fixing #2 (#19)
    * fixing #2
    
    * more fixes
    
    * fixing problems #3 solutions implmentes for groups/all
    
    * fix all problems momentaneo jeje #4
    
    * fixing all problems (mentira, solo los que nos generan conflictos momentaneos)
    
    ---------

--- controllers/movies.py
+++ controllers/movies.py
@@ -1,20 +1,21 @@
+from fastapi import HTTPException
 from sqlalchemy import text
 from controllers.moviesapi import api,base_url_movies,video,adult
 from controllers.session import add_to_db
 from models.Movie import Movie
 from config.db import session
+from schema.Movie import MovieCreate
+from fastapi import status
 
-def downloadMovie(language: str):
-
-    for page in range(1, api(base_url_movies)["total_pages"] + 1):
-        for movie in api(f"{base_url_movies}&page={page}")['results']:
-
-            existing_movie = session.query(Movie).filter(Movie.id == movie['id']).first()
-            if existing_movie != None:
-                existing_movie.title = {**existing_movie.get('title', {}), language: movie['title']}
-                existing_movie.synopsis = {**existing_movie.get('synopsis', {}), language: movie['overview']}                
-                session.commit()
-
+def downloadMovie(language: str,page:int, adult = adult,video= video):
+    if page > 500:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="the page limit is 500 :(")
+    cantidad_peliculas = 0
+    url = base_url_movies(adult,video,language)
+    for movie in api(f"{url}&page={page}")['results']:
+        cantidad_peliculas += 1
+        existing_movie = session.query(Movie).filter(Movie.id == movie['id']).first()
+        if existing_movie is None:
             add_to_db(Movie(
                 id=movie['id'],
                 adult=movie['adult'],
@@ -25,22 +26,47 @@ def downloadMovie(language: str):
                 image=movie['poster_path'],
                 release_date=movie['release_date'],
                 vote_average=movie['vote_average'],
-                vote_count=movie['vote_count']))
-
+                vote_count=movie['vote_count']
+            ))
+        else:
+            existing_movie.title = {**existing_movie.title, language: movie['title']}
+            existing_movie.synopsis = {**existing_movie.synopsis, language: movie['overview']}
+            session.commit()
     if video:
-        return downloadMovie(language, adult, video=False)
-
+        return downloadMovie(language, page, video=False, adult=adult)
     elif adult:
-        return downloadMovie(language, adult=False, video=True)
-    
-    return session.query(Movie).all()
+        return downloadMovie(language, page, video=video, adult=False)
+    return {"message":f"download great! {cantidad_peliculas}"}
 
-def get_all_movies(idiom, page=1, items_per_page=100):
+def get_all_movies(idiom, page=1, items_per_page=20):
     start = (page - 1) * items_per_page
     end = start + items_per_page
     movies = session.query(Movie.id, text(f"movies.title->>'{idiom}'")).slice(start, end).all()
-    return [{'id': movie[0], 'title': movie[1]} for movie in movies]
+    list_movie = []
+    for movie in movies:
+        if movie[1] is None:
+            continue
+        list_movie.append(get_movie_by_id(movie.id,idiom))
+    contador = page + 1
+    if len(list_movie) < 19:
+        downloadMovie(idiom,contador)
+        return get_all_movies(idiom,contador)
+    return {f"peliculas mostrando : {len(list_movie)}":list_movie}
 
 def get_movie_by_id(id, idiom):
-    movie = session.query(Movie.id, text(f"movies.title->>'{idiom}'")).filter(Movie.id == id).first()
-    return {'id': movie[0], 'title': movie[1]} if movie else None
+    movie = session.query(Movie).filter(Movie.id == id).first()
+    if movie is not None:
+        return MovieCreate(
+                id = movie.id,
+                adult = movie.adult,
+                title = movie.title[idiom],
+                genre_ids = movie.genre_ids,
+                language = movie.language,
+                synopsis = movie.synopsis[idiom],
+                image = movie.image,
+                release_date = movie.release_date,
+                vote_average = movie.vote_average,
+                vote_count = movie.vote_count
+        )
+    return ""
+   
