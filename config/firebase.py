61602be72cacd5f8187eeaf312f60c1699025ec7
Pablo Hurtado Gonzalo <79223844+Coshiloco@users.noreply.github.com>
2024-04-28 18:10:51 +0200

    Nombre Mal WIP (#24)

--- config/firebase.py
+++ config/firebase.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 
 def initialize_firebase():
     # Path to your Firebase Admin SDK key
-    path_to_key = '/etc/secrets/family-film-app-firebase-adminsdk-qr49b-762ab41ed8.json'
+    path_to_key = '/etc/secrets/JSON_FILE'
     
     cred = credentials.Certificate(path_to_key)
     firebase_app = firebase_admin.initialize_app(cred)
