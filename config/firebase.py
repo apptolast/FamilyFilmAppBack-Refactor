cad2e64d16253a92775678d4dfea7a9767bfd852
El3auti <126888203+El3auti@users.noreply.github.com>
2024-04-28 12:18:38 -0300

    endpoint grupo (create) fixeado (#21)
    * endpoint grupo (create) fixeado
    
    * Commit provisional para la implementacion de Firebase
    
    * Librerias de Firebase
    
    * Fix The Docker Containers
    
    * Funcion para las credenciales del servicio de Firebase SDk Admin
    
    * De momento esto
    
    * Entendiendo bien que son dos objetos distintos
    
    * Verificar token Firebase WIP
    
    * Verificar token WIP
    
    * Verificar Token con Firebase WIP
    
    ---------
    
    Co-authored-by: El3auti <elbautisocial@gmail.com>
    Co-authored-by: Coshiloco <pablohurtadohg@gmail.com>

---
+++ config/firebase.py
@@ -0,0 +1,10 @@
+import firebase_admin
+from firebase_admin import credentials
+
+def initialize_firebase():
+    # Path to your Firebase Admin SDK key
+    path_to_key = 'family-film-app-firebase-adminsdk-qr49b-762ab41ed8.json'
+    
+    cred = credentials.Certificate(path_to_key)
+    firebase_app = firebase_admin.initialize_app(cred)
+    return firebase_app
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from config.firebase import initialize_firebase
 from router.genres import router as genres_router
 from router.groups import router as groups_router
 from router.movies import router as movies_router
@@ -10,6 +11,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# Init Firebase Admin SDK and get the app
+initialize_firebase()
 
 app.include_router(genres_router)
 app.include_router(groups_router)
