f5a041c3c6c6f9711ba8e52c640ee9ac2fb20add
Coshiloco <pablohurtadohg@gmail.com>
2024-05-10 18:56:12 +0200

    In progress to automatic refresh token

--- schema/User.py
+++ schema/User.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from pydantic import BaseModel, EmailStr
 
+from config.readJsonUsers import read_json_users_file
+
 
 class UserSchemaRequest(BaseModel):
     email: EmailStr
@@ -10,6 +12,13 @@ class UserFirebaseBackendTestRequest(BaseModel):
     email: EmailStr
     email_verified: Optional[bool] = True
     password: str
+
+class AutomaticUpdateTokenRequest(BaseModel):
+    email: Optional[str] = read_json_users_file()
+
+class AutomaticUpdateTokenResponse(BaseModel):
+    email: str
+    token: str
 
 class UserTokenResponse(BaseModel):
     idToken:str
