d22c5048e11e96ef6d686c51a94c026b048b40fa
El3auti <bautistammateuci@gmail.com>
2024-05-10 11:24:15 -0300

    implements groups endpoints

--- controllers/Group.py
+++ controllers/Group.py
@@ -1,12 +1,14 @@
 from fastapi import HTTPException,status
+from sqlalchemy import and_
 from controllers.MovieGroupUser import MovieUserGroupService
 from models.Group import Group
+from models.MovieUserGroup import MovieUserGroup
 
 class GroupService:
         
-        def __init__(self, db_session):
-            self.db_session = db_session,
-            self.movie_user_group_repository = MovieUserGroupService(self.db_session)
+        def __init__(self, db_session,movie_user_group_repository):
+            self.movie_user_group_repository = movie_user_group_repository
+            self.db_session = db_session
 
         def create_group(self, name, ownerID):
             new_group = Group(
@@ -16,18 +18,49 @@ class GroupService:
             try:
                 self.db_session.add(new_group)
                 self.db_session.commit()
-                grupo = self.db_session.query(Group).filter(Group.owner_id == ownerID).all()[-1]
-                self.movie_user_group_repository.create_union_user(user_id=ownerID,group_id=grupo.id)
+                group = self.db_session.query(Group).filter(Group.owner_id == ownerID).all()[-1]
+                self.movie_user_group_repository.create_union_user(user_id=ownerID,group_id=group.id)
             except Exception as e:
                  self.db_session.rollback()
                  raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"{e}")
             
-            return grupo
+            return group
         
         def get_groups(self,user_id):
-            return MovieUserGroupService.get_groups(user_id=user_id)
+            return self.movie_user_group_repository.get_groups(user_id=user_id)
             
-        
         def get_group_id(self, gruop_id):
-            return MovieUserGroupService.get_group_id(gruop_id=gruop_id)
+            return self.movie_user_group_repository.get_group_id(gruop_id=gruop_id)
 
+        def add_user_to_group(self,user_id,group_id,owner_id):
+            try:
+                if self.is_owner(owner_id,group_id) is None:
+                    raise HTTPException(status_code=404, detail="User not Owner")
+
+                self.movie_user_group_repository.create_union_user(user_id,group_id)    
+                return self.db_session.query(MovieUserGroup).filter(MovieUserGroup.id_group == group_id).all()
+
+            except HTTPException as http_error:
+                raise http_error
+            
+        def delete_user_to_group(self,user_id,group_id,owner_id):
+            try:
+                if self.is_owner(owner_id,group_id) is None:
+                    raise HTTPException(status_code=404, detail="User not Owner")
+                
+
+                self.movie_user_group_repository.delete_union_user(user_id,group_id)    
+                return self.db_session.query(MovieUserGroup).filter(MovieUserGroup.id_group == group_id).all()
+
+            except HTTPException as http_error:
+                raise http_error
+
+
+        def is_owner(self,user_id,group_id):
+            try:
+                group = self.db_session.query(Group).filter(and_(Group.owner_id == user_id, Group.id == group_id)).first()
+                return group
+            except Exception as e:
+                 self.db_session.rollback()
+                 raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"User is not owner")
+           
