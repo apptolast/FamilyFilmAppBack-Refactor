0f2f2468e8b721c31df5bb02864251cf1babf67a
El3auti <bautistammateuci@gmail.com>
2024-05-12 15:19:10 -0300

    fix deployment

--- config/firebase.py
+++ config/firebase.py
@@ -3,8 +3,8 @@ from firebase_admin import credentials
 
 def initialize_firebase():
     # Path to your Firebase Admin SDK key
-    path_to_key = '/Users/bautistamateuci/Desktop/FamilyFilmAppBack-Refactor/usertest.json'
-    
+    path_to_key = '/etc/secrets/JSON_FILE'
+
     cred = credentials.Certificate(path_to_key)
     firebase_app = firebase_admin.initialize_app(cred)
     return firebase_app
--- schema/Group.py
+++ schema/Group.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from pydantic import BaseModel, EmailStr
 
+from schema.Movie import MovieResponse
+
 
 class GroupSchemaRequest(BaseModel):
     name:str
@@ -12,3 +14,10 @@ class UserSchemaResponse(BaseModel):
     name: str
     provider:str
 
+
+class groupSchema(BaseModel):
+    id:int
+    owner_id:int
+    users:list[EmailStr]
+    movie_toWatch:list[MovieResponse]
+    movie_Watched:list[MovieResponse]
