44884772dbaf61a9b6e797761ef1ad91943243cf
Coshiloco <pablohurtadohg@gmail.com>
2024-05-13 14:07:35 +0200

    Sistema de Logging para automated Token NO SE TOCA BAUTI WIP

--- config/readJsonUsers.py
+++ config/readJsonUsers.py
@@ -2,7 +2,7 @@ import json
 
 def read_json_users_file():
     data = any
-    with open('usertest.json', 'r') as file:
+    with open('/etc/secrets/USER_TEST', 'r') as file:
         data = json.load(file)
         new_data = data['users']
         first_user = new_data[0]['email']
