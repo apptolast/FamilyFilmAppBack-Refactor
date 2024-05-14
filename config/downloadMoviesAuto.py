ab9e2761ea166ea8501ba28a7eb8b99fa0c54354
Coshiloco <pablohurtadohg@gmail.com>
2024-05-14 17:43:51 +0200

    Cambios para saber qie esta pasando WIP

--- config/downloadMoviesAuto.py
+++ config/downloadMoviesAuto.py
@@ -8,4 +8,7 @@ def automated_download_movies():
     url = os.getenv("URL_FOR_AUTOMATIC_DOWNLOAD_MOVIES")
     headers = {'Content-Type': 'application/json'}
     response = requests.get(url, headers=headers)
-    #logging.info("Respuesta automática del endpoint de descarga de peliculas: %s", response.json())
+    if response.status_code == 200:
+        return response.json()
+    else:
+        logging.info("Respuesta automática del endpoint de descarga de peliculas funcion que se ejecuta automatica : %s     , ERROR : %s", response.status_code, response.text)
