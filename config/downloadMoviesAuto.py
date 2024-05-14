2e205b27e9bb1b2257cc99386447c39da6c6e050
Coshiloco <pablohurtadohg@gmail.com>
2024-05-14 17:57:05 +0200

    Para VER QUE PASA AMS INFORMACIÓN DE DEBUGGING

--- config/downloadMoviesAuto.py
+++ config/downloadMoviesAuto.py
@@ -8,7 +8,13 @@ def automated_download_movies():
     url = os.getenv("URL_FOR_AUTOMATIC_DOWNLOAD_MOVIES")
     headers = {'Content-Type': 'application/json'}
     response = requests.get(url, headers=headers)
+    logging.info(f"HTTP Status Code: {response.status_code}")
+    logging.info(f"Response Text: {response.text}")
     if response.status_code == 200:
-        return response.json()
+        try:
+            response_json = response.json()
+            logging.info("Respuesta automática del endpoint de descarga de peliculas: %s", response_json)
+        except requests.exceptions.JSONDecodeError:
+            logging.error(f"Error decoding JSON from response: {response.text}")
     else:
-        logging.info("Respuesta automática del endpoint de descarga de peliculas funcion que se ejecuta automatica : %s     , ERROR : %s", response.status_code, response.text)
+        logging.error("Respuesta automática del endpoint de descarga de peliculas: %s     , ERROR : %s", response.status_code, response.text)
