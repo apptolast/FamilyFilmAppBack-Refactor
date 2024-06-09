0d6f90c8eb986ff54cfa3d5d3e4e11e1d24d9a79
bauti <bautistammateuci@gmail.com>
2024-06-09 20:45:35 -0300

    show details error for endpoint edit group

--- router/groups.py
+++ router/groups.py
@@ -23,7 +23,7 @@ async def create_group(group:GroupSchemaRequest,me = Depends(UserServiceReposito
 @router.put("/{id}")
 async def edit_group_name(group:GroupSchemaRequest,me = Depends(UserServiceRepository.auth_user)):
     user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
-    GroupRepository.edit_group_name(name = group.name,ownerID = user.id)
+    GroupRepository.edit_group_name(name = group.name,ownerID = user.id,group_id=id)
     return returns.get_groups(user.id,returns.get_user_id(user.id).language)
 
 
