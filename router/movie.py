2e5333afa3f05a29c6f68ab1c8bc637d37fd1ce2
El3auti <bautistammateuci@gmail.com>
2024-07-27 16:11:43 -0300

    tiramos produccion con esto fijo

--- router/movie.py
+++ router/movie.py
@@ -14,20 +14,21 @@ router = APIRouter(
 MovieServiceRepository = MovieService(session)
 
 returns = DataTransfer()
-@router.get("/{page}/{leng}")
+@router.get("/{page}")
 async def get_movies(page:int,leng:str):
-    return returns.get_movies_datatransfer(leng,page)
+    user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
+    return returns.get_movies_datatransfer(returns.get_user_id(user.id).language,page)
 
 @router.get("/{id}")
 async def get_movies(id:int,me = Depends(UserServiceRepository.auth_user)):
     user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
     return returns.get_movie_datatransfer(id,returns.get_user_id(user.id).language)
 
-@router.get('/update/movies/automated', status_code=200, response_model=AutomaticResponseForMovies)
-async def updated_movies_endpoint():
-    try:
-        response = DataTransfer().call_to_update_movies_peer_week(genre_service=DataTransfer().GenreServiceRepository)
-        return response
-    except Exception as e:
-        logging.error(f"Error updating movies: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+# @router.get('/update/movies/automated', status_code=200, response_model=AutomaticResponseForMovies)
+# async def updated_movies_endpoint():
+#     try:
+#         response = DataTransfer().call_to_update_movies_peer_week(genre_service=DataTransfer().GenreServiceRepository)
+#         return response
+#     except Exception as e:
+#         logging.error(f"Error updating movies: {e}")
+#         raise HTTPException(status_code=500, detail="Internal Server Error")
