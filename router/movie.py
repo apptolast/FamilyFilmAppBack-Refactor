bbe5cb835a0954b7ebb0cdbe4e5fc833ae69a0bc
El3auti <bautistammateuci@gmail.com>
2024-07-28 13:18:24 -0300

    datatransfer correcciones

--- router/movie.py
+++ router/movie.py
@@ -17,7 +17,6 @@ returns = DataTransfer()
 @router.get("/{page}")
 async def get_movies(page:int,me = Depends(UserServiceRepository.auth_user)):
     user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
-    print(f"LENGUAGE ES {returns.get_user_id(user.id).language}")
     MovieServiceRepository.dowload_movie(returns.get_user_id(user.id).language,page)
     return returns.get_movies_datatransfer(returns.get_user_id(user.id).language,page)
 
