79d20df7e7fa5baebdb624f3c01d3577be177510
El3auti <126888203+El3auti@users.noreply.github.com>
2024-07-27 22:19:39 -0300

    vamos con esto

--- router/genre.py
+++ router/genre.py
@@ -9,9 +9,17 @@ router = APIRouter(
 )
 
 GenreServiceRepository = GenreService(session)
+returns = DataTransfer()
+
+@router.post("")
+async def dowload_genres(me = Depends(UserServiceRepository.auth_user)):
+    user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
+    return GenreServiceRepository.dowload_genres(returns.get_user_id(user.id).language)
 
 @router.get("/{idiom}")
-async def get_genres(idiom:str):
+async def get_genres(idiom:str,me = Depends(UserServiceRepository.auth_user)):
+    user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
+
     return GenreServiceRepository.get_genres(idiom)
 
 
