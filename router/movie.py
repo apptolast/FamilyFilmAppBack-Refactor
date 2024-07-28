855e77c80fced13efa0bc991c12ed0ff43e27427
El3auti <126888203+El3auti@users.noreply.github.com>
2024-07-27 22:01:45 -0300

    fix movie done :)

--- router/movie.py
+++ router/movie.py
@@ -15,7 +15,7 @@ MovieServiceRepository = MovieService(session)
 
 returns = DataTransfer()
 @router.get("/{page}")
-async def get_movies(page:int):
+async def get_movies(page:int,me = Depends(UserServiceRepository.auth_user):
     user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
     print(f"USER {user}")
     return returns.get_movies_datatransfer(returns.get_user_id(user.id).language,page)
