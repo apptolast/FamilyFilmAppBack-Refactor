47f97fe00906c65a1235f38276e352b079a9c09f
El3auti <bautistammateuci@gmail.com>
2024-07-28 14:46:02 -0300

    download_movie implementing...

--- router/movie.py
+++ router/movie.py
@@ -17,10 +17,10 @@ returns = DataTransfer()
 @router.get("/{page}")
 async def get_movies(page:int,me = Depends(UserServiceRepository.auth_user)):
     user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
-    MovieServiceRepository.dowload_movie(returns.get_user_id(user.id).language,page)
+    MovieServiceRepository.download_movie(returns.get_user_id(user.id).language,page)
     return returns.get_movies_datatransfer(returns.get_user_id(user.id).language,page)
 
-@router.get("movie/{id}")
+@router.get("/{id}")
 async def get_movies(id:int,me = Depends(UserServiceRepository.auth_user)):
     user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
     return returns.get_movie_datatransfer(id,returns.get_user_id(user.id).language)
