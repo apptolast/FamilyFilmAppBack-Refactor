4264601455cd54fb311775719f113de8c98dc3b4
El3auti <126888203+El3auti@users.noreply.github.com>
2024-07-27 22:25:44 -0300

    import

--- router/genre.py
+++ router/genre.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from config.db import session
 from controllers.Genre import GenreService
 from controllers.DataTransfer import DataTransfer
+from router.users import UserServiceRepository
 
 router = APIRouter(
     prefix="/genres",
@@ -10,6 +11,7 @@ router = APIRouter(
 
 GenreServiceRepository = GenreService(session)
 returns = DataTransfer()
+
 @router.post("")
 async def dowload_genres(me = Depends(UserServiceRepository.auth_user)):
     user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
