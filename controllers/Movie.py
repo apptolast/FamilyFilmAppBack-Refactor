ec0df1f77b6df451a5f41ad7f3e684aa4c3839c3
El3auti <bautistammateuci@gmail.com>
2024-07-29 10:44:37 -0300

    datatransfer trabaja bien, falla movie_download, testing..

--- controllers/Movie.py
+++ controllers/Movie.py
@@ -38,75 +38,29 @@ class MovieService:
             self.db_session.rollback()
             raise HTTPException(status_code=500, detail=f"An error occurred:  {str(e)}")
     
-    # def download_movie(self, language, page, adult=True, video=True):
-    #     print(f"EL LENGUAGE EN LA FUNCION DOWNLOAD ES {language}")
-
-    #     if page > 500:
-    #         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="The page limit is 500")
-
-    #     movie_downloads = []
-    #     url = f"https://api.themoviedb.org/3/discover/movie?include_adult={adult}&include_video={video}&language={language}&sort_by=popularity.desc&page={page}"
-    #     print(f"Request URL: {url}")
-
-    #     # Fetch the movies
-    #     response = self.api_start(url)
-    #     movies = response.get('results', [])
-    #     print(f"Movies fetched: {len(movies)}")
-
-    #     for movie in movies:
-    #         existing_movie = self.db_session.query(Movie).filter(Movie.id == movie['id']).first()
-    #         print(f"Checking movie: {movie['id']}")
-
-    #         if existing_movie is None:
-    #             # Add new movie if it doesn't exist
-    #             print(f"Adding new movie: {movie['title']}")
-    #             movie_downloads.append(movie)
-    #             new_movie = Movie(
-    #                 id=movie['id'],
-    #                 title={language: movie['title']},
-    #                 synopsis={language: movie['overview']},
-    #                 image=movie['poster_path'],
-    #                 adult=movie['adult'],
-    #                 release_date=movie['release_date'],
-    #                 rating_average=movie['vote_average'],
-    #                 rating_value=movie['vote_count']
-    #             )
-    #             self.db_session.add(new_movie)
-    #             self.db_session.commit()
-    #             self.set_genres_with_movie(movie)
-
-    #         existing_movie.title[language] = movie['title']
-    #         print(f"CAMBIAMOS TITULO AHORA TENEMOS ESTOS TITULOS {existing_movie.title}")
-    #         print(f"CAMBIAMOS descripcion AHORA TENEMOS ESTOS descripcionS {existing_movie.synopsis}")
-    #         existing_movie.synopsis[language] = movie['overview']
-    #         self.db_session.commit()
-
-    #     if video:
-    #         print(f"Fetching movies with video set to False")
-    #         return self.download_movie(language, page, video=False, adult=adult)
-    #     elif adult:
-    #         print(f"Fetching movies with adult set to False")
-    #         return self.download_movie(language, page, video=video, adult=False)
-
-    #     return movie_downloads
-
     def download_movie(self, language, page, adult=True, video=True):
+        print(f"EL LENGUAJE EN LA FUNCION DOWNLOAD ES {language}")
 
         if page > 500:
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="The page limit is 500")
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="The page limit is 500")
 
         movie_downloads = []
         url = f"https://api.themoviedb.org/3/discover/movie?include_adult={adult}&include_video={video}&language={language}&sort_by=popularity.desc&page={page}"
+        print(f"Request URL: {url}")
 
         # Fetch the movies
         response = self.api_start(url)
         movies = response.get('results', [])
+        print(f"Movies fetched: {len(movies)}")
 
         for movie in movies:
             existing_movie = self.db_session.query(Movie).filter(Movie.id == movie['id']).first()
-            movie_downloads.append(movie)
+            print(f"Checking movie: {movie['id']}")
+            movie_downloads.append(new_movie)
+
             if existing_movie is None:
                 # Add new movie if it doesn't exist
+                print(f"Adding new movie: {movie['title']}")
                 new_movie = Movie(
                     id=movie['id'],
                     title={language: movie['title']},
@@ -118,34 +72,46 @@ class MovieService:
                     rating_value=movie['vote_count']
                 )
                 self.db_session.add(new_movie)
+
+                # Commit all changes once after all movies are processed
+                try:
+                    self.db_session.commit()
+                    print("All changes committed successfully.")
+                except Exception as e:
+                    self.db_session.rollback()
+                    print(f"Error committing changes: {e}")
+
             else:
                 # Update existing movie's title and synopsis
-                if not isinstance(existing_movie.title, dict):
-                    existing_movie.title = {}
-                if not isinstance(existing_movie.synopsis, dict):
-                    existing_movie.synopsis = {}
+                title_data = existing_movie.title if isinstance(existing_movie.title, dict) else {}
+                synopsis_data = existing_movie.synopsis if isinstance(existing_movie.synopsis, dict) else {}
 
-                existing_movie.title[language] = movie['title']
-                existing_movie.synopsis[language] = movie['overview']
+                title_data[language] = movie['title']
+                synopsis_data[language] = movie['overview']
 
+                existing_movie.title = title_data
+                existing_movie.synopsis = synopsis_data
 
-            # Commit the changes after each loop iteration
-            try:
-                self.db_session.commit()
-            except Exception as e:
-                self.db_session.rollback()
-                print(f"Error committing changes: {e}")
-                continue
+                print(f"Updated title: {existing_movie.title}")
+                print(f"Updated synopsis: {existing_movie.synopsis}")
 
-        # if video:
-        #     print(f"Fetching movies with video set to False")
-        #     return self.download_movie(language, page, video=False, adult=adult)
-        # elif adult:
-        #     print(f"Fetching movies with adult set to False")
-        #     return self.download_movie(language, page, video=video, adult=False)
+                try:
+                    self.db_session.commit()
+                    print("All changes committed successfully.")
+                except Exception as e:
+                    self.db_session.rollback()
+                    print(f"Error committing changes: {e}")
+            
+
+        if video:
+            print(f"Fetching movies with video set to False")
+            return self.download_movie(language, page, video=False, adult=adult)
+        elif adult:
+            print(f"Fetching movies with adult set to False")
+            return self.download_movie(language, page, video=video, adult=False)
 
         return movie_downloads
-
+    
     def set_genres_with_movie(self,movie):
         genres = movie['genre_ids']
         movie_id = movie['id']
