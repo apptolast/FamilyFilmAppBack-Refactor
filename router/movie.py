9666b35a941fb7a5076c547ccbeea0fb1695601a
El3auti <bautistammateuci@gmail.com>
2024-07-29 12:36:56 -0300

    add get_movie_name

--- router/movie.py
+++ router/movie.py
@@ -5,6 +5,7 @@ from controllers.Movie import MovieService
 from controllers.DataTransfer import DataTransfer
 from router.users import UserServiceRepository
 from schema.Movie import AutomaticResponseForMovies
+from schema.MovieUserGroup import MovieSearchName
 
 router = APIRouter(
     prefix="/movies",
@@ -22,9 +23,15 @@ async def get_movies(page:int,me = Depends(UserServiceRepository.auth_user)):
 
 
 @router.get("/{id}")
-async def get_movies(id:int,me = Depends(UserServiceRepository.auth_user)):
+async def get_movie_id(id:int,me = Depends(UserServiceRepository.auth_user)):
     user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
     return returns.get_movie_datatransfer(id,returns.get_user_id(user.id).language)
+
+@router.get("name/{page}")
+async def get_movie_name(Movie_name:MovieSearchName,page:int,me = Depends(UserServiceRepository.auth_user)):
+    user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
+    MovieServiceRepository.download_movie_by_name(returns.get_user_id(user.id).language,Movie_name,page)
+    return [ returns.get_movie_datatransfer(movie['id'],returns.get_user_id(user.id).language) for movie in MovieServiceRepository.download_movie_by_name(returns.get_user_id(user.id).language,Movie_name,page)]
 
 # @router.get('/update/movies/automated', status_code=200, response_model=AutomaticResponseForMovies)
 # async def updated_movies_endpoint():
