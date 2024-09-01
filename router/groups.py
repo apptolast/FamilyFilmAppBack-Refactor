7e29d287dc16f03108232d64bcf236267a9d45ad
JustBauti <bautistammateuci@gmail.com>
2024-09-01 20:03:20 -0300

    fix id_movie

--- router/groups.py
+++ router/groups.py
@@ -76,13 +76,13 @@ async def group_delete_to_watch_movie(group_id:int,movieIds:MovieIds,me = Depend
     GroupRepository.delete_to_watch(user.id,group_id=group_id,movie_id=movieIds.ids_movie)
     return returns.get_groups(user.id,returns.get_user_id(user.id).language)
 
-@router.put("/{group_id}/ToWatched/{ids_movie}")
+@router.put("/{group_id}/ToWatched/")
 async def group_add_to_watched_movie(group_id:int,movieIds:MovieIds,me = Depends(UserServiceRepository.auth_user)):
     user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
     GroupRepository.add_to_watched(user.id,group_id,movieIds.ids_movie)
     return returns.get_groups(user.id,returns.get_user_id(user.id).language)
 
-@router.delete("/{group_id}/ToWatched/{ids_movie}")
+@router.delete("/{group_id}/ToWatched/")
 async def group_delete_to_watched_movie(group_id:int,movieIds:MovieIds,me = Depends(UserServiceRepository.auth_user)):
     user = UserServiceRepository.get_user_id(user_id= UserServiceRepository.check_user_exists(me['email']).id)
     GroupRepository.delete_to_watched(user.id,group_id=group_id,movie_id=movieIds.ids_movie)
