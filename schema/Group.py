107769c7fb8884c653580ae0dc77c43f2104a110
JustBauti <bautistammateuci@gmail.com>
2024-09-02 19:26:11 -0300

    test #1

--- schema/Group.py
+++ schema/Group.py
@@ -26,7 +26,7 @@ class groupSchema(BaseModel):
     users:list[usuarito]
     watch: list[MovieResponse] = []
     watched: list[MovieResponse] = []
-    # recommended_movie: MovieResponse = None
+    recommended_movie: MovieResponse = None
 
 class groupSchemaDeleteUser(BaseModel):
     email: str
